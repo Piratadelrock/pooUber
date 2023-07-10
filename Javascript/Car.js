a343538edf539e9c80f087377a13c664164afa9c
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2023-07-09 22:28:41 -0500

    se crea constructor para javascript en conductor  y se crea constructor para la clase Account MetodoConstructor

--- Javascript/Car.js
+++ Javascript/Car.js
@@ -1,6 +1,15 @@
-function Car() {
+function Car(license, driver) {
   this.id;
-  this.license;
-  this.driver;
+  this.license = license;
+  this.driver = driver;
   this.passegenger;
 }
+/* con este podemos crear un metodo a
+partir de la function Car
+*/
+Car.prototype.printDataCar = function () {
+  console.log(this.driver);
+  console.log(this.driver.name);
+  console.log(this.driver.document);
+  // console.log("License: " + this.license + " Name Driver: " + this.driver);
+};
---
+++ Javascript/index.js
@@ -0,0 +1,4 @@
+// declarando
+var car = new Car("AW213", new Account("Andres", "123ASD"));
+car.passegenger = 4;
+car.printDataCar();
