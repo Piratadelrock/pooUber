4defee4efb9230235e1f65a11135b26be7cca116
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2023-07-15 00:27:31 -0500

    agregando la herencia de la clase Car a sus subclases de la superclase

--- PHP/UberX.php
+++ PHP/UberX.php
@@ -4,4 +4,13 @@ class UberX extends Car
 {
   public $brand;
   public $model;
+
+  public function __construct($license, $driver, $brand, $model)
+  {
+    // pasando los parametros a la superclase como en java super aca seria parent
+    parent::__construct($license, $driver);
+
+    $this->brand = $brand;
+    $this->model = $model;
+  }
 }
