9ce08165b1b68dff055690e2a256abf2225ed085
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2023-07-15 00:10:53 -0500

    creando herencia de la clase Car (creando entidades de los Uber[tipo])

---
+++ Java/UberVan.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Map;
+
+class UberVan extends Car {
+
+    // map:representa la marca array: representara el modelo
+    Map<String, Map<String, Integer>> typeCarAccepted;
+
+    // leather or vinyl
+    ArrayList<String> seatsMaterial;
+
+    public UberVan(
+            String license, Account driver,
+            Map<String, Map<String, Integer>> typeCarAccepted,
+            ArrayList<String> seatsMaterial) {
+
+        super(license, driver);
+
+        this.typeCarAccepted = typeCarAccepted;
+        this.seatsMaterial = seatsMaterial;
+    }
+
+}
